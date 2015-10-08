3a8b124db70da8aee6608717112b7fc890b86e29
Maurizio De Magnis <root@olisti.co>
2015-10-08 12:22:21 +0200

    Handles the embedded shaders with BabelJS

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Threetwenty
 320 is a simple game library written in javascript.
 
 You can see a couple live demos at [alexnava.github.io](http://alexnava.github.io)
+
+## BabelJS
+
+Install [BabelJS](https://babeljs.io/docs/setup/) and then launch the watcher to that `.es6` files get automatically compiled into `.js`:
+
+```bash
+$ babel --extensions '.es6' --watch js/ --out-dir js/
+```
--- js/webgl-shaders.js
+++ js/webgl-shaders.js
@@ -1,111 +1,17 @@
-// A very simple vertex shader
-var BasicVertexShader = "\
-	attribute vec4 aVertexPosition;\
-	attribute vec4 aVertexColor;\
-	\
-	uniform mat4 uMVMatrix;\
-	uniform mat4 uPMatrix;\
-	\
-	varying vec4 vColor;\
-	\
-	void main()\
-	{\
-		gl_Position = uPMatrix * uMVMatrix * aVertexPosition;\
-		vColor = aVertexColor;\
-	}";
+"use strict";
 
-// A very simple fragment shader
-var BasicFragmentShader = "\
-	precision mediump float;\
-	\
-	varying vec4 vColor;\
-	\
-	void main()\
-	{\
-		gl_FragColor = vColor;\
-	}";
+var BasicVertexShader = "\n\tattribute vec4 aVertexPosition;\n\tattribute vec4 aVertexColor;\n\n\tuniform mat4 uMVMatrix;\n\tuniform mat4 uPMatrix;\n\n\tvarying vec4 vColor;\n\n\tvoid main()\n\t{\n\t\tgl_Position = uPMatrix * uMVMatrix * aVertexPosition;\n\t\tvColor = aVertexColor;\n\t}\n";
+
+var BasicFragmentShader = "\n\tprecision mediump float;\n\n\tvarying vec4 vColor;\n\n\tvoid main()\n\t{\n\t\tgl_FragColor = vColor;\n\t}\n";
 
 // Simple vertex shader with texture support
-var BasicTextureVertexShader = "\
-	attribute vec4 aVertexPosition;\
-	attribute vec2 aTextureCoord;\
-	\
-	uniform mat4 uMVMatrix;\
-	uniform mat4 uPMatrix;\
-	varying vec2 vTextureCoord;\
-	\
-	void main()\
-	{\
-		gl_Position = uPMatrix * uMVMatrix * aVertexPosition;\
-		vTextureCoord = aTextureCoord;\
-	}";
+var BasicTextureVertexShader = "\n\tattribute vec4 aVertexPosition;\n\tattribute vec2 aTextureCoord;\n\n\tuniform mat4 uMVMatrix;\n\tuniform mat4 uPMatrix;\n\tvarying vec2 vTextureCoord;\n\n\tvoid main()\n\t{\n\t\tgl_Position = uPMatrix * uMVMatrix * aVertexPosition;\n\t\tvTextureCoord = aTextureCoord;\n\t}\n";
 
 // Simple fragment shader with texture support
-var BasicTextureFragmentShader = "\
-	precision mediump float;\
-	\
-	varying vec2 vTextureCoord;\
-	uniform sampler2D uSampler;\
-	uniform vec4 uBaseColor;\
-	\
-	void main()\
-	{\
-		vec4 fragmentColor;\
-		\
-		fragmentColor = texture2D(uSampler, vec2(vTextureCoord.s, vTextureCoord.t));\
-		gl_FragColor = fragmentColor * uBaseColor;\
-	}";
+var BasicTextureFragmentShader = "\n\tprecision mediump float;\n\n\tvarying vec2 vTextureCoord;\n\tuniform sampler2D uSampler;\n\tuniform vec4 uBaseColor;\n\n\tvoid main()\n\t{\n\t\tvec4 fragmentColor;\n\n\t\tfragmentColor = texture2D(uSampler, vec2(vTextureCoord.s, vTextureCoord.t));\n\t\tgl_FragColor = fragmentColor * uBaseColor;\n\t}\n";
 
 // Adaptation for centered quads (or any other geometry)
-var Quad2DTextureVertexShader = "\
-	attribute vec4 aVertexPosition;\
-	attribute vec2 aTextureCoord;\
-	\
-	uniform mat4 uPMatrix;\
-	uniform vec2 uCenterPosition;\
-	uniform float uScale;\
-	uniform float uRotation;\
-	varying vec2 vTextureCoord;\
-	\
-	/* Matrix expressed as columns */\
-	mat4 mvMatrix = mat4(\
-		uScale * cos(uRotation), uScale * sin(uRotation), 0.0, 0.0,\
-		-uScale * sin(uRotation), uScale * cos(uRotation), 0.0, 0.0,\
-		0.0, 0.0, 1.0, 0.0,\
-		uCenterPosition.x, uCenterPosition.y, 0.0, 1.0\
-	);\
-	\
-	void main()\
-	{\
-		gl_Position = uPMatrix * mvMatrix * aVertexPosition;\
-		vTextureCoord = aTextureCoord;\
-	}";
+var Quad2DTextureVertexShader = "\n\tattribute vec4 aVertexPosition;\n\tattribute vec2 aTextureCoord;\n\n\tuniform mat4 uPMatrix;\n\tuniform vec2 uCenterPosition;\n\tuniform float uScale;\n\tuniform float uRotation;\n\tvarying vec2 vTextureCoord;\n\n\t/* Matrix expressed as columns */\n\tmat4 mvMatrix = mat4(\n\t\tuScale * cos(uRotation), uScale * sin(uRotation), 0.0, 0.0,\n\t\t-uScale * sin(uRotation), uScale * cos(uRotation), 0.0, 0.0,\n\t\t0.0, 0.0, 1.0, 0.0,\n\t\tuCenterPosition.x, uCenterPosition.y, 0.0, 1.0\n\t);\n\n\tvoid main()\n\t{\n\t\tgl_Position = uPMatrix * mvMatrix * aVertexPosition;\n\t\tvTextureCoord = aTextureCoord;\n\t}\n";
 
 // Adaptation for aligned rectangles (like text...)
-var Rect2DTextureVertexShader = "\
-	attribute vec4 aVertexPosition;\
-	attribute vec2 aTextureCoord;\
-	\
-	uniform mat4 uPMatrix;\
-	uniform vec2 uCornerPosition; /* bottom left */\
-	uniform vec2 uSize; /* width, heigth */\
-	uniform vec2 uTextureCornerPosition; /* bottom left */\
-	uniform vec2 uTextureSelectionSize; /* width, heigth */\
-	uniform vec2 uTextureSize; /* width, heigth */\
-	varying vec2 vTextureCoord;\
-	\
-	/* Matrix expressed as columns */\
-	mat4 mvMatrix = mat4(\
-		uSize.x, 0.0, 0.0, 0.0,\
-		0.0, uSize.y, 0.0, 0.0,\
-		0.0, 0.0, 1.0, 0.0,\
-		uCornerPosition.x, uCornerPosition.y, 0.0, 1.0\
-	);\
-	\
-	void main()\
-	{\
-		gl_Position = uPMatrix * mvMatrix * aVertexPosition;\
-		vec2 normalizedTextureCorner = uTextureCornerPosition / uTextureSize;\
-		vec2 textureScale = uTextureSelectionSize / uTextureSize;\
-		vTextureCoord = normalizedTextureCorner + aTextureCoord * textureScale;\
-	}";
+var Rect2DTextureVertexShader = "\n\tattribute vec4 aVertexPosition;\n\tattribute vec2 aTextureCoord;\n\n\tuniform mat4 uPMatrix;\n\tuniform vec2 uCornerPosition; /* bottom left */\n\tuniform vec2 uSize; /* width, heigth */\n\tuniform vec2 uTextureCornerPosition; /* bottom left */\n\tuniform vec2 uTextureSelectionSize; /* width, heigth */\n\tuniform vec2 uTextureSize; /* width, heigth */\n\tvarying vec2 vTextureCoord;\n\n\t/* Matrix expressed as columns */\n\tmat4 mvMatrix = mat4(\n\t\tuSize.x, 0.0, 0.0, 0.0,\n\t\t0.0, uSize.y, 0.0, 0.0,\n\t\t0.0, 0.0, 1.0, 0.0,\n\t\tuCornerPosition.x, uCornerPosition.y, 0.0, 1.0\n\t);\n\n\tvoid main()\n\t{\n\t\tgl_Position = uPMatrix * mvMatrix * aVertexPosition;\n\t\tvec2 normalizedTextureCorner = uTextureCornerPosition / uTextureSize;\n\t\tvec2 textureScale = uTextureSelectionSize / uTextureSize;\n\t\tvTextureCoord = normalizedTextureCorner + aTextureCoord * textureScale;\n\t}\n";
